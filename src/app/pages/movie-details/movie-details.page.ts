369c724c03a46e5da01fb0faa1815d104c32043a
Your Name <severus.key308@gmail.com>
2022-11-19 23:12:37 +0400

    1 Com

---
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from 'src/app/services/movie.service';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-movie-details',
+  templateUrl: './movie-details.page.html',
+  styleUrls: ['./movie-details.page.scss'],
+})
+export class MovieDetailsPage implements OnInit { 
+  movie: any = null;
+  imageBaseUrl = environment.images
+
+  constructor(
+    private route: ActivatedRoute, 
+    private movieService: MovieService
+  ) { }
+
+  ngOnInit() {
+    const id: string | null  = this.route.snapshot.paramMap.get('id');
+    console.log(id)
+
+    this.movieService.getMovieDetails(id).subscribe((res) =>{
+      console.log(res);
+      this.movie = res;
+    })
+  }
+
+  openHomepage(){
+    window.open(this.movie.homepage);
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '7c0285c720d0b2e3bc9bbcf1d457b0d6',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p'
 };
 
 /*
